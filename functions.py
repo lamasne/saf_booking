66304b4276db3dc23409cd3463ccb65760a87915
Lamas Neil <neillamas@gmail.com>
2024-03-12 23:41:17 +0100

    Tested: able to spam until got the time of interest

--- functions.py
+++ functions.py
@@ -11,18 +11,31 @@ def random_delay():
     """
     return random.uniform(1, 2)
 
+
 def check_availability(time_string, driver, max_loading_time):
-    time.sleep(3) # Gives time for the right schedule to show
+    time.sleep(3)  # Gives time for the right schedule to show
     # Find the available sessions for today
-    available_sessions = WebDriverWait(driver, max_loading_time).until( # Wait for the element to be present
+    available_sessions = WebDriverWait(
+        driver, max_loading_time
+    ).until(  # Wait for the element to be present
         EC.presence_of_all_elements_located((By.CLASS_NAME, "hora-sininfo"))
-    )    
+    )
     for session in available_sessions:
         if time_string in session.text:  # Check if the session is at 17:15
-            time.sleep(random_delay())
+            time.sleep(0.2)
             try:
                 session.click()  # Click on the session
                 return True
             except Exception as e:
                 print("Not available")
                 return False
+
+
+def go_to_next_day(driver, max_loading_time):
+    next_day_button_ls = WebDriverWait(
+        driver, max_loading_time
+    ).until(  # Wait for the element to be present
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "next-day"))
+    )
+    time.sleep(random_delay())
+    next_day_button_ls[0].click()
--- main.py
+++ main.py
@@ -7,10 +7,13 @@ from selenium.common.exceptions import WebDriverException
 import time
 from functions import *
 
-max_loading_time = 10 # in seconds
+# Left to do: 1) instead of is_tomorrow use date 2) do the cancel 18:30 trick
+
+max_loading_time = 10  # in seconds
 time_of_interest = "17:15"
 is_for_tomorrow = True
-time_between_attempts = 5 # in seconds
+time_between_attempts = 5  # in seconds
+max_number_of_attempts = 1000
 
 # Set up Selenium WebDriver
 driver = webdriver.Chrome()  # Or choose the appropriate WebDriver for your browser
@@ -18,12 +21,16 @@ driver = webdriver.Chrome()  # Or choose the appropriate WebDriver for your brow
 try:
     # Navigate to the gym's website and log in
     driver.get("https://uab.deporsite.net/loginmenu")
-    username_input = WebDriverWait(driver, max_loading_time).until( # Wait for the element to appear
+    username_input = WebDriverWait(
+        driver, max_loading_time
+    ).until(  # Wait for the element to appear
         EC.presence_of_element_located((By.ID, "email"))
     )
     time.sleep(random_delay())
     username_input.send_keys("Y7645569S")
-    password_input = driver.find_element("id", "password")  # Locate the password input field
+    password_input = driver.find_element(
+        "id", "password"
+    )  # Locate the password input field
     time.sleep(random_delay())
     password_input.send_keys("85Isashy85")
     time.sleep(random_delay())
@@ -31,62 +38,95 @@ try:
 
     # Navigate to the booking page
     time.sleep(random_delay())
-    driver.get("https://uab.deporsite.net/reserva-espais?IdDeporte=531")  # Replace with your gym's website
-
-    # Go to tommorow's sessions
-    if is_for_tomorrow:
-        next_day_button_ls = WebDriverWait(driver, max_loading_time).until( # Wait for the element to be present
-            EC.presence_of_all_elements_located((By.CLASS_NAME, "next-day"))
-        )    
-        time.sleep(random_delay())
-        next_day_button_ls[0].click()
+    driver.get(
+        "https://uab.deporsite.net/reserva-espais?IdDeporte=531"
+    )  # Replace with your gym's website
 
     # Loop until time of interest becomes available
+    nb_of_attempts = 0
+    # Go to tommorow's sessions
+    if is_for_tomorrow:
+        go_to_next_day(driver, max_loading_time)
     availability_found = check_availability(time_of_interest, driver, max_loading_time)
     while not availability_found:
+        if nb_of_attempts >= max_number_of_attempts:
+            break
         driver.refresh()
+        # Go to tommorow's sessions
+        if is_for_tomorrow:
+            go_to_next_day(driver, max_loading_time)
+        availability_found = check_availability(
+            time_of_interest, driver, max_loading_time
+        )
+        nb_of_attempts += 1
         time.sleep(time_between_attempts)
-        availability_found = check_availability(time_of_interest, driver, max_loading_time)
 
     # Confirm the booking (assuming there's a button to confirm the booking)
-    confirm_button = WebDriverWait(driver, max_loading_time).until( # Wait for the element to be present
+    confirm_button = WebDriverWait(
+        driver, max_loading_time
+    ).until(  # Wait for the element to be present
         EC.presence_of_all_elements_located((By.CLASS_NAME, "btn-modal-horas"))
-    )            
+    )
     time.sleep(random_delay())
     confirm_button[0].click()
 
-    # Wait for agreement checkbox to be clickable 
-    agree_terms_check = WebDriverWait(driver, max_loading_time).until( # Wait for the element to be present
-        EC.presence_of_element_located((By.ID, "checkAceptacionCondiciones"))
-    )
-    # Scroll down to the checkbox
-    time.sleep(random_delay())
-    driver.execute_script("arguments[0].scrollIntoView();", agree_terms_check)
+    try:
+        # Wait for agreement checkbox to be clickable
+        agree_terms_check = WebDriverWait(
+            driver, max_loading_time
+        ).until(  # Wait for the element to be present
+            EC.presence_of_element_located((By.ID, "aceptacionCondiciones"))
+        )
+        # Scroll down to the checkbox
+        time.sleep(random_delay())
+        driver.execute_script("arguments[0].scrollIntoView();", agree_terms_check)
 
-    # Ensure the checkbox is visible and enabled
-    if agree_terms_check.is_displayed() and agree_terms_check.is_enabled():
         # Check the checkbox
         time.sleep(random_delay())
         agree_terms_check.click()
-    else:
-        raise WebDriverException("Checkbox is not visible or enabled")
 
-    # Click on continue
-    continue_button = WebDriverWait(driver, max_loading_time).until( # Wait for the element to be present
-        EC.element_to_be_clickable((By.ID, "btnSiguiente"))
-    )    
-    time.sleep(random_delay())
-    continue_button.click()
+        # Click on continue
+        continue_button = WebDriverWait(
+            driver, max_loading_time
+        ).until(  # Wait for the element to be present
+            EC.element_to_be_clickable((By.ID, "btnSiguiente"))
+        )
+        time.sleep(random_delay())
+        continue_button.click()
 
-except Exception as e:
-    print('Error:', e)
+        # Click on next continue
+        reservar_button = WebDriverWait(
+            driver, max_loading_time
+        ).until(  # Wait for the element to be present
+            EC.presence_of_all_elements_located(
+                (
+                    By.CSS_SELECTOR,
+                    "div.col-md-3:not(.hidden) .btn-siguiente-pagar:not(.hidden)",
+                )
+            )
+        )
+
+        # reservar_button = WebDriverWait(
+        #     driver, max_loading_time
+        # ).until(  # Wait for the element to be present
+        #     EC.element_to_be_clickable((By.CLASS_NAME, "btn-siguiente-pagar"))
+        # )
+        time.sleep(random_delay())
+        reservar_button[0].click()
+        print(f"You have successfully booked for tomorrow/today at {time_of_interest}")
+    except Exception as e:
+        time.sleep(120)
+        print("CANCEL THE BOOKING! You have 2 minutes")
+
+except Exception as e1:
+    print("Error:", e1)
 
 # Log out
-time.sleep(random_delay())
+time.sleep(5)  # Make sure the booking has been processed
 driver.get("https://uab.deporsite.net/logout")  # Replace with your gym's website
-time.sleep(3)  
-print('logged out')
+print("logged out")
 
 # Close the browser
+time.sleep(3)  # Make sure I have been logged out
 driver.quit()
-print('closed the window')
+print("closed the window")
